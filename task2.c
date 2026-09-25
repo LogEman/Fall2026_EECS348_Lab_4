fb0b82b1a4ed49d262230d5215ef0ec815497006
Logan Kessler <logankessler@ku.edu>
2026-09-25 15:47:30 -0500

    Conversion code written

--- task2.c
+++ task2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int original_temperature;
-    int converted_temperature;
+    double original_temperature;
+    double converted_temperature;
     char original_scale;
     char target_scale;
 
@@ -14,15 +14,31 @@ int main(){
     printf("Enter the scale to convert to (C, F, or K): \n");    
     scanf("%d", &target_scale);
 
-    if(original_scale == 'C'):
-        if(target_scale == 'F'):{
-
+    if(original_scale == 'C'){
+        if(target_scale == 'F'){
+            converted_temperature = (original_temperature * 1.8) + 32;
         }
-        elif(target_scale == 'K'):{
-
+        else if(target_scale == 'K'){
+            converted_temperature = original_temperature + 273.15;
         }
-    elif(original_scale == 'F'):
-        if()
+    }
+    else if(original_scale == 'F'){
+        if(target_scale == 'C'){
+            converted_temperature = (original_temperature - 32) * 1.8;
+        }
+        else if(target_scale == 'K'){
+            converted_temperature = ((original_temperature - 32) * 1.8) + 273.15;
+        }  
+    }
+    else if(original_scale == 'K'){
+        if(target_scale == 'F'){
+            converted_temperature = ((original_temperature - 273.15) * 1.8) + 32;
+        }
+        else if(target_scale == 'C'){
+            converted_temperature = original_temperature - 273.15;
+        }
+    }
+    
     printf("Converted temperature: \n");
     printf("Temperature category: \n");
     printf("Weather advisory: \n");
