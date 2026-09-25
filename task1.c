a1037da501c8e0e97d8fee09a248709d0d3c77a9
Logan Kessler <logankessler@ku.edu>
2026-09-25 15:35:27 -0500

    Began writing code for task2

--- task1.c
+++ task1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(){
+    const int touchdown = 6;
+    const int field_goal = 3;
+    const int safety = 2;
+    const int touchdown_conversion = 8;
+    const int touchdown_fieldgoal = 7;
 
+    int input;
 
-    return 0;
+printf("Enter the NFL score (Enter 1 to stop): \n");
+scanf("%d", &input);
+
+if(input > )
+printf("You entered: %d", input);
+return 0;
 }
--- task2.c
+++ task2.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
 int main(){
+    int original_temperature;
+    int converted_temperature;
+    char original_scale;
+    char target_scale;
 
 
+    printf("Enter the original temperature value: \n");
+    scanf("%d", &original_temperature);
+    printf("Enter the original scale (C, F, or K): \n");
+    scanf("%d", &original_scale);
+    printf("Enter the scale to convert to (C, F, or K): \n");    
+    scanf("%d", &target_scale);
+
+    if(original_scale == 'C'):
+        if(target_scale == 'F'):{
+
+        }
+        elif(target_scale == 'K'):{
+
+        }
+    elif(original_scale == 'F'):
+        if()
+    printf("Converted temperature: \n");
+    printf("Temperature category: \n");
+    printf("Weather advisory: \n");
     return 0;
 }
